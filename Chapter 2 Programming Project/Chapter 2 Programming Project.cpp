c203906ba40548ed2980be98a9bd557e082b5597
Jesse zheng <BessiieBoi@outlook.com>
2025-02-17 22:53:03 -0500

    Allowed program to loop and added an exit function

--- Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <limits>
-
-void clearinputs() {
-	std::cin.clear();
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
+#include <string>
 
 int currentYear = 2025;
 float oceanlvlIncrease = 1.5;
 
 int calcYearDiff()	{
 	int targetYear;
-	std::cout << "Enter target year for ocean level increase prediction.\n";
+	std::cout << "Enter target year for ocean level increase prediction or type 'exit'.\n";
 
 	while (true) {
 		if (std::cin >> targetYear) {
 			if (targetYear >= currentYear) {
 				return targetYear - currentYear;
 			} else {
-				clearinputs();
-				std::cout << "Target year must be greater than current year. Please enter valid target year.\n";
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				std::cout << "Target year must be greater than current year. Please enter valid target year or type 'exit'.\n";
 			}
 		} else {
-			clearinputs();
-			std::cout << "Invalid input. Please enter a valid year.\n";
+			std::string input;
+			std::cin.clear();
+			std::getline(std::cin, input);
+			if (input == "exit") {
+				return -1;
+			}
+			else {
+				std::cout << "Invalid input. Please enter a valid year or type 'exit'.\n";
+			}
 		}
 	}
 }
@@ -33,10 +37,15 @@ float calcPredictedOceanLevel(int yearDifference) {
 }
 
 int main() {
-	int yearDifference = calcYearDiff();
-	float predictedOceanLevel = calcPredictedOceanLevel(yearDifference);
-	std::cout << "The ocean level will be " << predictedOceanLevel
-		<< " cm higher than the current level in "
-		<< (currentYear + yearDifference) << "." << std::endl;
+	while (true) {
+		int yearDifference = calcYearDiff();
+		if (yearDifference == -1) {
+			break;
+		}
+		float predictedOceanLevel = calcPredictedOceanLevel(yearDifference);
+		std::cout << "The ocean level will be " << predictedOceanLevel
+			<< " cm higher than the current level in "
+			<< (currentYear + yearDifference) << "." << std::endl;
+	}
 	return 0;
 }
