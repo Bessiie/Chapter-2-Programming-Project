62c5fc97e9089f8ca7ce043900c7449910dc4534
Jesse zheng <BessiieBoi@outlook.com>
2025-02-18 19:37:23 -0500

    I think this is what the question wanted

--- Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
@@ -1,20 +1,20 @@
-// Chapter 2 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int surveyedCustomers = 16500;
+const float percentMultipleWeeklyPurchasers = .15;
+const float percentCitrusLovers = .58;
+
+float totalMultipleWeeklyPurchasers() {
+	return surveyedCustomers * percentMultipleWeeklyPurchasers;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+float totalCitrusLovers(float MultipleWeeklyPurchasers) {
+	return MultipleWeeklyPurchasers * percentCitrusLovers;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+	float MultipleWeeklyPurchasers = totalMultipleWeeklyPurchasers();
+	float CitrusLovers = totalCitrusLovers(MultipleWeeklyPurchasers);
+	std::cout << "The total amount of people who purchase one or more energy drinks per week: " << MultipleWeeklyPurchasers << "\n"
+		<< "The total amount of CitrusLovers: " << CitrusLovers << std::endl;
+}
