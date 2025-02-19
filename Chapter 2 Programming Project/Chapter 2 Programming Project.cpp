f4e29f1e673efd4f6a782c43e78ff7560afce355
Jesse zheng <BessiieBoi@outlook.com>
2025-02-18 19:39:59 -0500

    Deleted Hi-Yah

--- Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
@@ -1,3 +1,2 @@
-//Chapter 2 Programming Projects HI-YAHH
-//Each Project is in its own seperate branch HI-YAH
-//I am unsure what to leave in the branch HI-YAH
+//Chapter 2 Programming Projects 
+//Each Project is in its own seperate branch 
