00beaa3ca7c65ab37a7efbeafc21cec7af45aa3d
Jesse zheng <BessiieBoi@outlook.com>
2025-02-24 19:44:03 -0500

    Comments

--- Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
@@ -1,10 +1,17 @@
-
 #include <iostream>
 
+//constant for fuel tank size
+float sizeFuelTank = 20;
+float mpgCity = 23.5;
+float mpgHighway = 28.9;
+
 int main() {
-	float sizeFuelTank = 20;
-	float distanceOnCityMPG = 23.5 * sizeFuelTank;
-	float distanceOnHighwayMPG = 28.9 * sizeFuelTank;
+
+//Calculates and assigns to distance
+	float distanceOnCityMPG = mpgCity * sizeFuelTank;
+	float distanceOnHighwayMPG = mpgHighway * sizeFuelTank;
+
+//Display output
 	std::cout
 		<< "You would be able to travel "
 		<< distanceOnCityMPG
