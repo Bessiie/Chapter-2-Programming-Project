20dbf4d8ac96edc9f9fac18d410ff74efc09ca23
Jesse zheng <BessiieBoi@outlook.com>
2025-02-24 19:36:29 -0500

    Added Comments and fixed precision

--- Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project/Chapter 2 Programming Project.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
+#include <iomanip>
 
-const int surveyedCustomers = 16500;
+
+const float surveyedCustomers = 16500;
 const float percentMultipleWeeklyPurchasers = .15;
 const float percentCitrusLovers = .58;
 
+// Function to calculate the total number of customers who purchase one or more energy drinks per week
 float totalMultipleWeeklyPurchasers() {
 	return surveyedCustomers * percentMultipleWeeklyPurchasers;
 }
 
+// Function to calculate the total number of citrus lovers among multiple weekly purchasers
 float totalCitrusLovers(float MultipleWeeklyPurchasers) {
 	return MultipleWeeklyPurchasers * percentCitrusLovers;
 }
 
+//Main function for output
 int main() {
 	float MultipleWeeklyPurchasers = totalMultipleWeeklyPurchasers();
 	float CitrusLovers = totalCitrusLovers(MultipleWeeklyPurchasers);
-	std::cout << "The total amount of people who purchase one or more energy drinks per week: " << MultipleWeeklyPurchasers << "\n"
+	// Set the output precision to 0 decimal places (rounding to nearest whole number)
+	std::cout << std::fixed << std::setprecision(0);
+	// Output the results to the console
+	std::cout 
+		<< "The total amount of people who purchase one or more energy drinks per week: " << MultipleWeeklyPurchasers << "\n"
 		<< "The total amount of CitrusLovers: " << CitrusLovers << std::endl;
+	return 0;
 }
